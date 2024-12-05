f7fa97ad7d1a7157c373cbbe2615c5cb39bade08
faultypointer <faultypointer@proton.me>
2024-12-05 11:53:04 +0545

    8000 port no for running using cargo run

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port.");
     run(listener)?.await
 }
